e435c54d6cf01996b28dc83f5ece040b17a6843c
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2022-02-01 22:45:31 +0100

    add test for reducers and actions

---
+++ src/features/page/pageSlice.spec.ts
@@ -0,0 +1,18 @@
+import pageReducer, { PageState, setPage } from "./pageSlice";
+
+describe("page reducer", () => {
+  const initialState: PageState = {
+    value: 2,
+  };
+
+  it("should handle initial state", () => {
+    expect(pageReducer(undefined, { type: "unknown" })).toEqual({
+      value: 1,
+    });
+  });
+
+  it("should handle setPage", () => {
+    const actual = pageReducer(initialState, setPage(10));
+    expect(actual.value).toEqual(10);
+  });
+});
--- src/features/searchTerm/searchTermSlice.ts
+++ src/features/searchTerm/searchTermSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../../app/store";
 
-interface SearchTermState {
+export interface SearchTermState {
   term: string;
 }
 
